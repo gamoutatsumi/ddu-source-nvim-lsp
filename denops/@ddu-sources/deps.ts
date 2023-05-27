0af68eaed74bf029fcbfd08e7f664f1eea90cf66
uga-rosa <uga6603@gmail.com>
2023-05-27 23:49:50 +0900

    Create deps.ts to manage library

---
+++ denops/@ddu-sources/deps.ts
@@ -0,0 +1,7 @@
+export {
+  BaseSource,
+  type Context,
+  type Item,
+} from "https://deno.land/x/ddu_vim@v2.8.6/types.ts#^";
+export { type Denops, fn } from "https://deno.land/x/ddu_vim@v2.8.6/deps.ts#^";
+export type { ActionData } from "https://deno.land/x/ddu_kind_file@v0.4.1/file.ts#^";
